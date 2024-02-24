f1e686bc76457a71ff3a39177475661a4fc8b5b8
Maslov Victor <vmasloff@gmail.com>
2024-02-25 05:02:11 +0900

    Remove /build/* from git.

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vitor/cpp/skillfactory/homework_5.9
-# Build directory: /home/vitor/cpp/skillfactory/homework_5.9/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
